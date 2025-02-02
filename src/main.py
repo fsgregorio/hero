fac9a0c78e899e3c1249de437083e9e3b9604112
fsgregorio <fsgregorio92@gmail.com>
2025-02-02 15:26:23 -0300

    Update readme

--- src/main.py
+++ src/main.py
@@ -1,12 +1,20 @@
+"""
+Main module to initialize and run the FastAPI application.
+
+This module sets up the FastAPI application, includes API routes from the 'api' module,
+and runs the Uvicorn server when executed as the main module.
+"""
+
 import uvicorn
 from fastapi import FastAPI
-from .api import app as api_app
+from .api import app as api_app  # Import the API instance from the api module
 
-# Criando a instância principal do FastAPI
+# Create the main FastAPI application instance
 app = FastAPI()
 
-# Incluindo os endpoints definidos em api.py
+# Include the endpoints defined in the API module
 app.include_router(api_app.router)
 
 if __name__ == "__main__":
+    # Run the Uvicorn server to host the FastAPI application
     uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
